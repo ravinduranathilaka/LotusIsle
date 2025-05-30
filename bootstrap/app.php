a329f8c7ef9b257ab12cd8d78c865536bd16ff7d
Ravindu Ranathilaka <ravindu925@gmail.com>
2025-05-30 20:02:11 +0900

    workaround, still looking for solution

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(AdminMiddleware::class);
+        //$middleware->append(AdminMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
